b2ec3d5e47a98ded20017d1919cb1dd7fd685180
Matris12 <103265011+Matris12@users.noreply.github.com>
2022-04-08 18:56:19 +0430

    Add files via upload

---
+++ reg16.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+module reg16(
+  input clk, INR, LOAD,CLR,
+  input [15:0] IN,   
+  output reg [15:0] OUT
+    );
+    always @(posedge clk)  
+       if (CLR==1)   
+         OUT<=16'h0000;
+       else if (LOAD==1)  
+          OUT <= IN;
+       else if(INR==1)  
+          OUT <= OUT+1;
+   
+endmodule
